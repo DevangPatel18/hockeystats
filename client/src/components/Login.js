ffc5ac2ba82a5a60c8dc08bdbcd6d02d27da7efd
dev637 <devang713@gmail.com>
2019-01-30 15:14:02 -0500

    Setup Login page
    Added necessary props and functions to Login page.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,53 +1,86 @@
-import React from "react"
-import { Link } from 'gatsby'
-import { navigate } from '@reach/router'
+import React from 'react'
+import { Link, navigate } from 'gatsby'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { loginUser, clearErrors } from '../actions/authActions'
 
 class Login extends React.Component {
   state = {
-    username: ``,
-    password: ``
+    email: ``,
+    password: ``,
+    errors: {},
   }
 
-  handleUpdate = (event) => {
+  componentDidMount() {
+    // If logged in and user navigates to Login page, should redirect them to home
+    if (this.props.auth.isAuthenticated) {
+      navigate('/app/home')
+    }
+    this.props.clearErrors()
+  }
+
+  static getDerivedStateFromProps(nextProps) {
+    if (nextProps.errors) {
+      return {
+        errors: nextProps.errors,
+      }
+    }
+  }
+
+  handleUpdate = event => {
     this.setState({
       [event.target.name]: event.target.value,
     })
   }
 
-  login = async() => {
-    // const { username, password } = this.state
-    try {
-      navigate("/app/home")
-    } catch (err) {
-      console.log('error...: ', err)
+  login = e => {
+    e.preventDefault()
+
+    const userData = {
+      email: this.state.email,
+      password: this.state.password,
     }
+
+    this.props.loginUser(userData)
   }
 
   render() {
+    const { errors } = this.state
     return (
       <div>
         <h1>Sign In</h1>
-        <div style={styles.formContainer}>
-         <input
-            onChange={this.handleUpdate}
-            placeholder='Username'
-            name='username'
-            value={this.state.username}
-            style={styles.input}
-          />
+        <form noValidate onSubmit={this.login} style={styles.formContainer}>
+          <label htmlFor="email">
+            Email
+            <span style={styles.error}>{errors.email}</span>
+          </label>
           <input
             onChange={this.handleUpdate}
-            placeholder='Password'
-            name='password'
+            name="email"
+            type="email"
+            id="email"
+            value={this.state.email}
+            style={styles.input}
+          />
+          <label htmlFor="password">
+            Password
+            <span style={styles.error}>{errors.password}</span>
+          </label>
+          <input
+            onChange={this.handleUpdate}
+            name="password"
+            id="password"
             value={this.state.password}
-            type='password'
+            type="password"
+            autoComplete=""
             style={styles.input}
           />
           <div style={styles.button} onClick={this.login}>
             <span style={styles.buttonText}>Sign In</span>
           </div>
-        </div>
-        <Link to="/app/signup">Sign Up</Link><br />
+        </form>
+        <Link to="/app/signup">Sign Up</Link>
+        <br />
       </div>
     )
   }
@@ -55,17 +88,44 @@ class Login extends React.Component {
 
 const styles = {
   input: {
-    height: 40, margin: '10px 0px', padding: 7
+    height: 40,
+    margin: '10px 0px',
+    padding: 7,
   },
   formContainer: {
-    display: 'flex', flexDirection: 'column'
+    display: 'flex',
+    flexDirection: 'column',
   },
   button: {
-    backgroundColor: 'royalblue', padding: '15px 7px', cursor: 'pointer', textAlign: 'center', marginBottom: 10
+    backgroundColor: 'royalblue',
+    padding: '15px 7px',
+    cursor: 'pointer',
+    textAlign: 'center',
+    marginBottom: 10,
   },
   buttonText: {
-    color: 'white'
-  }
+    color: 'white',
+  },
+  error: {
+    color: 'red',
+    fontSize: '0.7em',
+    float: 'right',
+  },
 }
 
-export default Login
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  clearErrors: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors,
+})
+
+export default connect(
+  mapStateToProps,
+  { loginUser, clearErrors }
+)(Login)
