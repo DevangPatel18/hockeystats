9f20f16c25b41a81f7367931d8cbd67474b43eb2
dev637 <devang713@gmail.com>
2019-02-03 12:16:48 -0500

    Apply wider container for pages with statistics
    Used Location component to grab page url for checking pages with statistics and adjusting container width accordingly. Reason for change is to utilize empty space to display more data.

--- client/src/components/layout.js
+++ client/src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import { Location } from '@reach/router'
 
 import Header from './header'
 import './layout.css'
@@ -18,28 +19,35 @@ const Layout = ({ children, data }) => (
       }
     `}
     render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
-      </>
+      <Location>
+        {({ location }) => {
+          const stats = location.pathname.includes('stats')
+          return (
+            <>
+              <Helmet
+                title={data.site.siteMetadata.title}
+                meta={[
+                  { name: 'description', content: 'Sample' },
+                  { name: 'keywords', content: 'sample, something' },
+                ]}
+              >
+                <html lang="en" />
+              </Helmet>
+              <Header siteTitle={data.site.siteMetadata.title} />
+              <div
+                style={{
+                  margin: '0 auto',
+                  maxWidth: stats ? 'auto' : 960,
+                  padding: '0px 1.0875rem 1.45rem',
+                  paddingTop: 0,
+                }}
+              >
+                {children}
+              </div>
+            </>
+          )
+        }}
+      </Location>
     )}
   />
 )
