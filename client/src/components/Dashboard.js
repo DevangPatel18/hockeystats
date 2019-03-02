9b394fff4811003590748855d10b166ce11758c6
dev637 <devang713@gmail.com>
2019-03-02 10:19:28 -0500

    Add lifecycle methods to Dashboard component
    Added lifecycle methods to Dashboard component and displayed tracked player ids from logged in user or localStorage.

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,20 +1,82 @@
 import React, { Component } from 'react'
 import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { getPlayerList } from '../actions/statActions'
 
 class Dashboard extends Component {
+  constructor() {
+    super()
+    this.state = {
+      trackedPlayers: [],
+    }
+
+    this._isMounted = false
+  }
+
+  componentDidMount() {
+    this._isMounted = true
+
+    if (this.props.auth.isAuthenticated) {
+      this.props.getPlayerList(this.props.auth.user.id)
+    } else {
+      if (localStorage.hasOwnProperty('players')) {
+        this.setState({
+          trackedPlayers: JSON.parse(localStorage.getItem('players')),
+        })
+        window.addEventListener(
+          'beforeunload',
+          this.playersToLocalStorage.bind(this)
+        )
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false
+
+    if (!this.props.auth.isAuthenticated && !this.props.auth.loading) {
+      window.removeEventListener(
+        'beforeunload',
+        this.playersToLocalStorage.bind(this)
+      )
+      this.playersToLocalStorage()
+    }
+  }
+
+  playersToLocalStorage() {
+    localStorage.setItem('players', JSON.stringify(this.state.trackedPlayers))
+  }
+
+  static getDerivedStateFromProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      return {
+        trackedPlayers: [...nextProps.stats.trackedPlayers],
+      }
+    }
+    return null
+  }
+
   render() {
+    const { trackedPlayers } = this.state
+
     return (
-      <div>
-        Dashboard
+      <div style={{ fontFamily: 'Arial' }}>
+        <h1>Dashboard</h1>
+        {trackedPlayers.length > 0 ? (
+          trackedPlayers.map(playerId => <div key={playerId}>{playerId}</div>)
+        ) : (
+          <div>No players selected for tracking.</div>
+        )}
+        <br />
+        <div>
+          <Link to="/app/playerstats">
+            <span>Player Statistics</span>
+          </Link>
+        </div>
         <div>
           <Link to="/">
-            <span
-              style={{
-                marginTop: '1rem',
-              }}
-            >
-              Back to Home
-            </span>
+            <span>Back to Home</span>
           </Link>
         </div>
       </div>
@@ -22,4 +84,18 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard
+Dashboard.propTypes = {
+  getPlayerList: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  stats: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  stats: state.stats,
+})
+
+export default connect(
+  mapStateToProps,
+  { getPlayerList }
+)(Dashboard)
