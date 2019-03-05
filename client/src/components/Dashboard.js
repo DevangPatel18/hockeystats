e50ec7954856cd446fe14df1eb3d7ab720596b0f
dev637 <devang713@gmail.com>
2019-03-05 18:53:18 -0500

    Setup request for player data in Dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,16 +3,35 @@ import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { getPlayerList } from '../actions/statActions'
+import configure from '../utils/configLocalforage'
+
+// Configure 'localforage' and instantiate 'axios' with 'axios-cache-adapter'
+configure()
 
 class Dashboard extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      trackedPlayerData: [],
+    }
 
     this._isMounted = false
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+    const { trackedPlayers } = this.props.stats
+
+    if (trackedPlayers.length) {
+      await configure().then(async api => {
+        const trackedPlayerData = await Promise.all(
+          trackedPlayers.map(playerId =>
+            api.get(`/api/statistics/players/${playerId}`).then(res => res.data)
+          )
+        )
+        this.setState({ trackedPlayerData })
+      })
+    }
+
     this._isMounted = true
 
     if (!this.props.auth.isAuthenticated) {
@@ -41,17 +60,39 @@ class Dashboard extends Component {
   }
 
   render() {
+    const { trackedPlayerData } = this.state
     const { trackedPlayers } = this.props.stats
 
     return (
       <div style={{ fontFamily: 'Arial' }}>
         <h1>Dashboard</h1>
-        {trackedPlayers.length > 0 ? (
-          trackedPlayers.map(playerId => <div key={playerId}>{playerId}</div>)
+        <br />
+        {trackedPlayers.length ? (
+          trackedPlayerData.map(playerObj => (
+            <div key={playerObj.id} style={{ marginBottom: '1rem' }}>
+              <h4>{playerObj.fullName}</h4>
+              <div>
+                Team: {playerObj.currentTeam.name} -{' '}
+                {playerObj.primaryPosition.abbreviation} - #{
+                  playerObj.primaryNumber
+                }
+              </div>
+              <div>
+                Bio: {playerObj.currentAge} yrs - {playerObj.height} ft -{' '}
+                {playerObj.weight} lbs
+              </div>
+              <div>Birthdate: {playerObj.birthDate}</div>
+              <div>
+                Birthplace: {playerObj.birthCity},
+                {playerObj.birthStateProvince &&
+                  ` ${playerObj.birthStateProvince}, `}{' '}
+                {playerObj.birthCountry}
+              </div>
+            </div>
+          ))
         ) : (
           <div>No players selected for tracking.</div>
         )}
-        <br />
         <div>
           <Link to="/app/playerstats">
             <span>Player Statistics</span>
