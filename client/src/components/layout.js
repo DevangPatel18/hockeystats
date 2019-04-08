146f3503e55382e808f17da90471957c219fb1e8
dev637 <devang713@gmail.com>
2019-04-08 13:50:04 -0400

    Fix full width style in layout component

--- client/src/components/layout.js
+++ client/src/components/layout.js
@@ -38,9 +38,8 @@ const Layout = ({ children, data }) => (
               <div
                 style={{
                   margin: '0 auto',
-                  maxWidth: full ? 'auto' : 960,
-                  padding: '0px 1.0875rem 1.45rem',
-                  paddingTop: 0,
+                  maxWidth: full ? '100%' : 960,
+                  padding: '1rem 1rem 1.5rem',
                 }}
               >
                 {children}
