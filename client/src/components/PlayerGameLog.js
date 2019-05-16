1186a252efd085b28fc5a5b121251c8daa7d0a2e
dev637 <devang713@gmail.com>
2019-05-16 15:50:54 -0400

    Add game log data to PlayerGameLog

--- client/src/components/PlayerGameLog.js
+++ client/src/components/PlayerGameLog.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react'
 import { AppBar, IconButton, Toolbar } from '@material-ui/core/'
 import CloseIcon from '@material-ui/icons/Close'
+import configure from '../utils/configLocalforage'
 import { yearFormatter } from '../helper/columnLabels'
 
 class PlayerGameLog extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      playerGameLogData: [],
+    }
+
+    this._isMounted = false
+  }
+
+  async componentDidMount() {
+    const { playerObj } = this.props
+    const { playerId, seasonId } = playerObj
+
+    configure().then(async api => {
+      const playerGameLogData = await api
+        .get(
+          `/api/statistics/players/gameLog/playerId/${playerId}/seasonId/${seasonId}/dataType/regular`
+        )
+        .then(res => res.data.reverse())
+
+      this.setState({ playerGameLogData })
+    })
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false
   }
 
   render() {
     const { onClose, playerObj } = this.props
+    const { playerGameLogData } = this.state
+
+    console.log(playerGameLogData)
 
     return (
       <div>
