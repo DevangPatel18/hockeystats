cb7bd9136a8367d725851fe6f31f15eabc45bcd7
dev637 <devang713@gmail.com>
2019-07-24 17:04:24 -0400

    Remove unnecessary import and argument

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,4 @@
 import API from '../utils/api'
-import axios from 'axios'
 import setAuthToken from '../utils/setAuthToken'
 import jwt_decode from 'jwt-decode'
 import { getPlayerList } from '../actions/statActions'
