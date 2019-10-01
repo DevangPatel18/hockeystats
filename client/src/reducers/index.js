df80d3635953bc98fb5a30981b0dc829b8fd3912
dev637 <devang713@gmail.com>
2019-10-01 16:53:31 -0400

    Add reducer for tableSettings

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 import authReducer from './authReducers'
 import errorReducer from './errorReducers'
 import statReducer from './statReducers'
+import tableSettingsReducer from './tableSettingsReducers'
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
   stats: statReducer,
+  tableSettings: tableSettingsReducer,
 })
---
+++ client/src/reducers/tableSettingsReducers.js
@@ -0,0 +1,14 @@
+const initialState = {
+  yearStart: '20182019',
+  yearEnd: '20182019',
+  reportName: 'skatersummary',
+  playoffs: false,
+  isAggregate: false,
+}
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
