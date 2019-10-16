9144cae7e3ec544bb7b328237a483fa212eee2e0
dev637 <devang713@gmail.com>
2019-10-16 13:08:35 -0400

    Add playerDataReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,9 +3,12 @@ import authReducer from './authReducers'
 import errorReducer from './errorReducers'
 import statReducer from './statReducers'
 import tableSettingsReducer from './tableSettingsReducers'
+import playerDataReducer from './playerDataReducers'
+
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
   stats: statReducer,
   tableSettings: tableSettingsReducer,
+  playerData: playerDataReducer,
 })
---
+++ client/src/reducers/playerDataReducers.js
@@ -0,0 +1,13 @@
+const initialState = {
+  stats: [],
+  teams: '',
+  countries: '',
+  dataType: '',
+}
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
