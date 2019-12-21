03bc0e56bf9073eca2f49c1a7450ee191457b981
dev637 <devang713@gmail.com>
2019-12-21 14:27:59 -0500

    Add button for PlayerOverview in PlayerProfiles

--- client/src/components/Dashboard/PlayerProfiles.js
+++ client/src/components/Dashboard/PlayerProfiles.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Trail, animated } from 'react-spring/renderprops'
 import PropTypes from 'prop-types'
 import IconButton from '@material-ui/core/IconButton'
-import { TableChart, Close } from '@material-ui/icons'
+import { TableChart, Equalizer, Close } from '@material-ui/icons'
 import { connect } from 'react-redux'
 import {
   ProfileContainer,
@@ -95,6 +95,14 @@ const PlayerProfiles = ({
                   children={<TableChart />}
                   style={{ padding: '0', marginLeft: '0.5rem' }}
                   onClick={() => openPlayerModal('gameLogModal', userData)}
+                />
+              </PlayerBioListItem>
+              <PlayerBioListItem>
+                Player overview ▶
+                <IconButton
+                  children={<Equalizer />}
+                  style={{ padding: '0', marginLeft: '0.5rem' }}
+                  onClick={() => openPlayerModal('overviewModal', playerObj)}
                 />
               </PlayerBioListItem>
             </PlayerBioList>
