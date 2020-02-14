5de73d429575577f68e3e10848dcdc8e44d5415b
dev637 <devang713@gmail.com>
2020-02-14 11:10:04 -0500

    Create GoalieSeasonsCharts component

---
+++ client/src/components/PlayerOverview/GoalieSeasonsCharts.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import {
+  VictoryChart,
+  VictoryGroup,
+  VictoryBar,
+  VictoryAxis,
+  VictoryLabel,
+  VictoryVoronoiContainer,
+} from 'victory'
+import chartTheme from '../../helper/chartTheme'
+
+class GoalieSeasonsCharts extends Component {
+  state = {
+    chartWidth: window.innerWidth - (window.innerWidth % 100),
+    chartHeight: window.innerHeight * 0.6 - ((window.innerHeight * 0.6) % 100),
+    wins: [],
+    losses: [],
+  }
+
+  componentDidMount() {
+    const { stats } = this.props
+    const playerStats = stats.playerObj.stats[0].splits.filter(
+      obj => obj.league.id === 133
+    )
+
+    let wins = []
+    let losses = []
+
+    let tempYear = ''
+    playerStats.forEach(obj => {
+      if (obj.season === tempYear) {
+        wins[wins.length - 1].y += obj.stat.wins
+        losses[losses.length - 1].y += obj.stat.losses
+      } else {
+        wins.push({ x: obj.season, y: obj.stat.wins })
+        losses.push({ x: obj.season, y: obj.stat.losses })
+        tempYear = obj.season
+      }
+    })
+
+    this.setState({ wins, losses })
+
+    window.addEventListener('resize', this.handleChartResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleChartResize)
+  }
+
+  handleChartResize = () => {
+    const { chartWidth, chartHeight } = this.state
+    const newWidth = window.innerWidth - (window.innerWidth % 100)
+    const newHeight =
+      window.innerHeight * 0.6 - ((window.innerHeight * 0.6) % 100)
+    if (newWidth !== chartWidth || newHeight !== chartHeight) {
+      this.setState({
+        chartWidth: newWidth,
+        chartHeight: newHeight,
+      })
+    }
+  }
+
+  render() {
+    const { chartWidth, chartHeight, wins, losses } = this.state
+    const theme = chartTheme(false, chartWidth, chartHeight)
+
+    if (wins.length < 1) return ''
+    return (
+      <div style={{ padding: '0 1rem' }}>
+        <VictoryChart
+          theme={theme}
+          containerComponent={
+            <VictoryVoronoiContainer
+              voronoiDimension="x"
+              labels={({ childName, _y }) => `${childName}: ${_y}`}
+            />
+          }
+        >
+          <VictoryGroup offset={20} colorScale={'qualitative'}>
+            <VictoryBar data={wins} name="wins" />
+            <VictoryBar data={losses} name="losses" />
+          </VictoryGroup>
+          <VictoryAxis
+            crossAxis
+            style={{
+              label: 'Label',
+              axisLabel: { padding: 0, angle: 35 },
+              tickLabels: { padding: 0, angle: 35 },
+            }}
+          />
+          <VictoryAxis dependentAxis crossAxis />
+          <VictoryLabel
+            angle="-90"
+            text={'Games'}
+            textAnchor="middle"
+            style={{ fontWeight: 'bolder' }}
+            x={10}
+            y={chartHeight / 2 - 25}
+          />
+          <VictoryLabel
+            text={'Season'}
+            textAnchor="middle"
+            style={{ fontWeight: 'bolder' }}
+            x={chartWidth / 2}
+            y={chartHeight - 10}
+          />
+        </VictoryChart>
+      </div>
+    )
+  }
+}
+
+GoalieSeasonsCharts.propTypes = {
+  stats: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = state => ({
+  stats: state.stats,
+})
+
+export default connect(mapStateToProps)(GoalieSeasonsCharts)
